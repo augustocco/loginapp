53f8bc615085d10fda504af7893fcec026ec88f0
Andrea Hernandez AH <andreah@formactiva.local>
2018-07-30 13:49:13 -0500

    Firebase Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyBBWQN13Ce8K3eAnaa-uK_BzY1QZ8ot6Kk",
+    authDomain: "loginapp-1837d.firebaseapp.com",
+    databaseURL: "https://loginapp-1837d.firebaseio.com",
+    projectId: "loginapp-1837d",
+    storageBucket: "",
+    messagingSenderId: "1049741379462"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyBBWQN13Ce8K3eAnaa-uK_BzY1QZ8ot6Kk",
+    authDomain: "loginapp-1837d.firebaseapp.com",
+    databaseURL: "https://loginapp-1837d.firebaseio.com",
+    projectId: "loginapp-1837d",
+    storageBucket: "",
+    messagingSenderId: "1049741379462"
+  }
 };
 
 /*
